f05d7de9ee04d9159f4b5e70aac58d732362c58d
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-01-02 18:30:59 +0500

    update

---
+++ components/footer.php
@@ -0,0 +1,95 @@
+<footer class="footer-one">
+    <div class="footer-one__bg" style="background-image: url(assets/images/backgrounds/footer-v1-bg.html);">
+    </div><!-- /.footer-one__bg -->
+    <div class="footer-one__top">
+        <div class="container">
+            <div class="row">
+                <!--Start Footer Widget Column-->
+                <div class="col-xl-4 col-lg-4 col-md-4 wow animated fadeInUp" data-wow-delay="0.1s">
+                    <div class="footer-widget__column footer-widget__about">
+                        <div class="footer-widget__about-logo">
+                            <a href="/"><img class="main-logo-img" src="assets/images/logo-white-v2.png" alt=""></a>
+                           <p> At Global Publishing Agency, we're eager to hear from you and explore how we can bring your literary vision to life. Whether you're ready to embark on a ghostwriting journey, have questions about our services, or want to discuss the details of your project, our team is here to assist you.</p>
+                        </div>
+                    </div>
+                </div>
+                <!--End Footer Widget Column-->
+
+                <!--Start Footer Widget Column-->
+                <div class="col-xl-2 col-lg-2 col-md-2 wow animated fadeInUp" data-wow-delay="0.3s">
+                    <div class="footer-widget__column footer-widget__courses">
+                        <h3 class="footer-widget__title">Services</h3>
+                        <ul class="footer-widget__courses-list list-unstyled">
+                            <li><a href="book-writing-service.html">Book Writing &amp; Publishing</a></li>
+                            <li><a href="book-editing-proofreading-service.html">Book Editing &amp; Proofreading</a></li>
+                            <li><a href="ghost-writing-service.html">Ghost Book Writing</a></li>
+                            <li><a href="audio-book-service.html">Audio Book Service</a></li>
+                            <li><a href="book-cover-designing.html"> Book Cover Design </a></li>
+                            <li><a href="book-illustrations-designing.html">  Book Illustrations  </a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-2 col-md-2 wow animated fadeInUp" data-wow-delay="0.3s">
+                    <div class="footer-widget__column footer-widget__courses">
+                        <h3 class="footer-widget__title">Services</h3>
+                        <ul class="footer-widget__courses-list list-unstyled">
+                            <li><a href="book-publishing-service.html">   Book Publishing   </a></li>
+                            <li><a href="book-printing-service.html">   Book Printing    </a></li>
+                            <li><a href="book-marketing-service.html">    Book Marketing     </a></li>
+                            <li><a href="author-website-development.html">    Author Website     </a></li>
+                            <li><a href="amazon-publishing-service.html">    Amazon Publishing     </a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!--End Footer Widget Column-->
+
+                <!--Start Footer Widget Column-->
+                <div class="col-xl-2 col-lg-2 col-md-2 wow animated fadeInUp" data-wow-delay="0.3s">
+                    <div class="footer-widget__column footer-widget__courses">
+                        <h3 class="footer-widget__title">Useful Links</h3>
+                        <ul class="footer-widget__courses-list list-unstyled">
+                            <li><a href="terms-and-conditions.html">Terms and Conditions</a></li>
+                            <li><a href="privacy-policy.html">Privacy Policy</a></li>
+                            <li><a href="contact.html">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!--End Footer Widget Column-->
+
+
+
+                <!--Start Footer Widget Column-->
+                <div class="col-xl-2 col-lg-2 col-md-2 wow animated fadeInUp" data-wow-delay="0.7s">
+                    <div class="footer-widget__column footer-widget__contact">
+                        <h3 class="footer-widget__title">Contact</h3>
+                        <!--<p class="text">600 California St, San Francisco, CA 94108, United States-->
+                        <!--</p>-->
+                        <p><a href="mailto:#">info@golbalpublishingagency.com</a></p>
+                        <p class="phone"><a href="tel:+123456">+123456</a></p>.
+
+                        <img src="assets/images/US-Payments-2.png" alt="">
+                    </div>
+                </div>
+                <!--End Footer Widget Column-->
+
+
+            </div>
+        </div>
+    </div>
+
+    <!--Start Footer One Bottom-->
+    <div class="footer-one__bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="footer-one__bottom-inner">
+                        <div class="footer-one__bottom-text text-center">
+                            <p>&copy; Copyright 2023 by Global Publishing Agency</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--End Footer One Bottom-->
+</footer>
