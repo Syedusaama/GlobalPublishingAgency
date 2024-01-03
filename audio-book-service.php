9fb7ad2b0ddae885b78c0b1cfa2ce5e2e3f93d14
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-01-03 19:44:27 +0500

    update

---
+++ audio-book-service.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<?php include "./components/head.php"?> 
+</head>
+
+<body>
+    <!-- /.preloader -->
+    <?php include "./components/header.php"?> 
+        <div class="stricky-header stricked-menu main-menu">
+            <div class="sticky-header__content">
+            </div>
+        </div>
+<section class="main-banner-custom bg-service-paper">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 col-md-7">
+                <h1> <span> Transform your book into an unforgettable auditory experience. </span></h1>
+                <p> Bespoke audiobook services for authors ready to give their words a voice.</p>
+                <img src="assets/images/banner-logo.png" alt="">
+
+                <div class="contact-btns">
+                    <a href="javascript:void(Tawk_API.toggle())">Chat With Expert</a>
+                </div>
+            </div>
+
+            <div class="col-lg-5 col-lg-5">
+            </div>
+        </div>
+    </div>
+</section>
+<section class="welcome-one mt-100">
+    <div class="container">
+        <div class="row">
+            <!--Start Welcome One Left-->
+            <div class="col-xl-6">
+                <div class="welcome-one__left">
+                    <div class="inner-section">
+                        <span class="section-title__tagline">Let us publish your Paper</span>
+                        <h2 class="inner-pages-title">Turning your words into captivating audiobooks</h2>
+                    </div>
+                    <p class="inner-pages-paragraph">Audiobooks make your stories available and accessible to a broader audience, including those who prefer to listen on the go or have limited time to read. They provide a new dimension to your narrative with voice narration, adding depth and emotion that transcends the written word.<br><br>At Global Publishing Agency, we understand the power of storytelling, not just through words on a page but through the captivating experience of audio. Our Audiobook Services are meticulously crafted to transform your written work into an immersive auditory journey. Explore the world of audiobooks with us and give your story a voice that resonates with your audience.
+                    </p>
+
+                    <div class="inner-pages-btn">
+                        <a href="javascript:void(Tawk_API.toggle())" class="thm-btn">Get A Free Quote</a>
+                    </div>
+                </div>
+            </div>
+            <!--End Welcome One Left-->
+
+            <!--Start Welcome One Right-->
+            <div class="col-xl-6">
+                <div class="welcome-one__right clearfix">
+                    <div class="shape1 rotate-me"><img src="assets/images/shapes/thm-shape1.png" alt=""></div>
+                    <div class="wow slideInRight animated" data-wow-delay="100ms" data-wow-duration="2500ms" style="visibility: visible; animation-duration: 2500ms; animation-delay: 100ms; animation-name: slideInRight;">
+                        <div class="inner-pages-img">
+                            <img src="assets/images/researcher2.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--End Welcome One Right-->
+        </div>
+    </div>
+</section>
+
+<!--Features One Start-->
+<section class="features-two bg-paper-pub">
+    <div class="inner-section text-center">
+        <span class="section-title__tagline">We Have Experts For All</span>
+        <h2 class="inner-pages-title-white">Embark on a literary journey that transcends pages</h2>
+    </div>
+    <div class="container">
+        <div class="row justify-content-center ">
+            <!--Start Single Features One-->
+            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
+                <div class="features-one__single">
+                    <div class="features-one__single-icon">
+                        <span class="icon-empowerment"></span>
+                    </div>
+                    <div class="features-one__single-text">
+                        <h4><a href="#!">Professional Narration</a></h4>
+                        <p>
+                            At Global Publishing Agency, our professional narrators are the storytellers who breathe life into your narrative. Each skilled narrator is chosen not just for their clear diction but for their ability to infuse emotion and expression into every word. They bring a depth of understanding to your story.</p>
+                            <br><br>
+                    </div>
+                </div>
+            </div>
+            <!--End Single Features One-->
+
+            <!--Start Single Features One-->
+            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1500ms">
+                <div class="features-one__single">
+                    <div class="features-one__single-icon">
+                        <span class="icon-human-resources-1"></span>
+                    </div>
+                    <div class="features-one__single-text">
+                        <h4><a href="#!">Studio-Quality Recording</a></h4>
+                        <p>
+                            We employ cutting-edge recording equipment to capture not just words but the very soul of your narrative. The result is studio-quality audio that brings out the richness and clarity of every spoken word. Our commitment to delivering an unparalleled listening experience means your audiobook is not just heard; it's felt.</p>
+                            <br><br>
+                    </div>
+                </div>
+            </div>
+            <!--End Single Features One-->
+
+            <!--Start Single Features One-->
+            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="400ms" data-wow-duration="1500ms">
+                <div class="features-one__single">
+                    <div class="features-one__single-icon">
+                        <span class="icon-recruitment"></span>
+                    </div>
+                    <div class="features-one__single-text">
+                        <h4><a href="#!">Custom Sound Design</a></h4>
+                        <p>
+                            Take your audiobook to the next level with our custom sound design service. We go beyond narration, adding atmospheric elements that elevate the mood and immerse listeners in the world you've created. Our sound designers craft an auditory landscape that complements and enhances the thematic elements.</p>
+                    </div>
+                </div>
+            </div>
+            <!--End Single Features One-->
+
+            <!--Start Single Features One-->
+            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1500ms">
+                <div class="features-one__single">
+                    <div class="features-one__single-icon">
+                        <span class="icon-human-resources-1"></span>
+                    </div>
+                    <div class="features-one__single-text">
+                        <h4><a href="#!">Editing and Post-Production</a></h4>
+                        <p>
+                            Our dedicated team handles the intricacies of editing and post-production with a commitment to perfection. From seamlessly blending narration and sound elements to refining pacing and ensuring a consistent tone, we polish every sonic detail. The result is a final product that not only meets industry standards but surpasses them. </p>
+                        <br><br>
+                    </div>
+                </div>
+            </div>
+            <!--End Single Features One-->
+
+            <!--Start Single Features One-->
+            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="400ms" data-wow-duration="1500ms">
+                <div class="features-one__single">
+                    <div class="features-one__single-icon">
+                        <span class="icon-recruitment"></span>
+                    </div>
+                    <div class="features-one__single-text">
+                        <h4><a href="#!">Premium Editing</a></h4>
+                        <p>
+                            Our premium editing service is designed to improve the overall quality of your research paper. Our team of experienced editors reviews your paper for grammar, spelling, syntax, and style errors. We also provide feedback on the structure, organization, and clarity of your paper to ensure that it meets the highest academic standards.</p> <br>
+                    </div>
+                </div>
+            </div>
+            <!--End Single Features One-->
+
+            <!--Start Single Features One-->
+            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="400ms" data-wow-duration="1500ms">
+                <div class="features-one__single">
+                    <div class="features-one__single-icon">
+                        <span class="icon-recruitment"></span>
+                    </div>
+                    <div class="features-one__single-text">
+                        <h4><a href="#1">Pre-submission Peer Review</a></h4>
+                        <p>
+                            Getting feedback from experts in your field can significantly improve the quality and impact of your research paper. Our pre-submission peer review service involves having your paper reviewed by a panel of experts in your research area. They provide feedback on the originality, clarity, and relevance of your research, helping you to improve your paper before submission.</p>
+                    </div>
+                </div>
+            </div>
+            <!--End Single Features One-->
+        </div>
+    </div>
+</section>
+<!--Features One End-->
+
+<!--Content Section-->
+<section class="about-two">
+    <div class="container">
+        <div class="about-two__bottom-content plain-cnt">
+            <div class="row">
+                <div class="col-xl-7 col-lg-7">
+                    <div class="about-two__title-box">
+                        <div class="inner-section">
+                            <span class="section-title__tagline">Professional Publishing Services </span>
+                            <h2 class="inner-pages-title">Why choose our audiobook services?
+                            </h2>
+                        </div>
+
+                        <p class="inner-pages-paragraph">In a world where multitasking is a way of life, audiobooks offer a unique and convenient way for readers to engage with your narrative. Through professional narration, studio-quality recording, custom sound design, and rigorous post-production, we transform your narrative into an audiobook that resonates, captivates, and stays with your audience long after the last word is spoken. Navigate the audiobook creation process with transparency. We keep you informed at every stage, from narration selection to the final production, ensuring your satisfaction.</p>
+                    </div>
+
+                    <ul class="list-unstyled">
+                        <li class="why-choose-one__left-list-single">
+                            <div class="icon">
+                                <span class="icon-confirmation"></span>
+                            </div>
+                            <div class="text">
+                                <p>On-time delivery</p>
+                            </div>
+                        </li>
+
+                        <li class="why-choose-one__left-list-single">
+                            <div class="icon">
+                                <span class="icon-confirmation"></span>
+                            </div>
+                            <div class="text">
+                                <p>24/7 support and assistance.</p>
+                            </div>
+                        </li>
+
+                        <li class="why-choose-one__left-list-single">
+                            <div class="icon">
+                                <span class="icon-confirmation"></span>
+                            </div>
+                            <div class="text">
+                                <p>Affordable packages.</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-xl-5 col-lg-5">
+                    <div class="domain-search-wrap border gray-light-bg" style="margin: 0;">
+                        <h4 class="text-center">Register Now | 50% Discount?</h4>
+                        <div class="form-banner">
+                            <div class="the-form-banner">
+    <div class="the-form_head">
+        <!-- <h2 class="pulse">EARLY BIRD DISCOUNTS</h2>
+        <h5>We are here to help you ace your academics.</h5> -->
+    </div>
+    <?php include "./components/registrationform.php"?> 
+</section>
+<!--About Two End-->
+
+<!--Portfolio Start-->
+
+<?php include "./components/portprocess.php"?> 
+
+<!--Portfolio Start-->
+
+
+<!--<div class="floating_btn">-->
+<!--    <a target="_blank" href="https://wa.me/+16164693925/?text=Hello, I need help in my publications">-->
+<!--      <div class="contact_icon">-->
+<!--           <img src="assets/images/whatsapp.png" class="my-float" alt="">-->
+
+<!--      </div>-->
+<!--</a></div>-->
+
+
+<!--Start Footer One-->
+<?php include "./components/footer.php"?> 
+<!--End Footer One-->
+</body>
+</html>
