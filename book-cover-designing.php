4e07210b35b436c0bce800cd0e901f27011aef6f
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-01-03 21:11:13 +0500

    update

---
+++ book-cover-designing.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<?php include "./components/head.php"?> 
+</head>
+
+<body>
+    <!-- /.preloader -->
+    <?php include "./components/header.php"?>
+        <div class="stricky-header stricked-menu main-menu">
+            <div class="sticky-header__content">
+            </div>
+        </div>
+<section class="main-banner-custom bg-service-paper">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 col-md-7">
+                <h1> <span> Crafted for impact, tailored for your narrative journey. </span></h1>
+                <p> Your story deserves a captivating first impression and visual expression in every imaginative detail. </p>
+                <img src="assets/images/banner-logo.png" alt="">
+                <div class="contact-btns">
+                    <a href="javascript:void(Tawk_API.toggle())">Chat With Expert</a>
+                </div>
+            </div>
+            <div class="col-lg-5 col-lg-5">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="welcome-one mt-100">
+    <div class="container">
+        <div class="row">
+            <!--Start Welcome One Left-->
+            <div class="col-xl-6">
+                <div class="welcome-one__left">
+                    <div class="inner-section">
+                        <span class="section-title__tagline">Let us publish your Paper</span>
+                        <h2 class="inner-pages-title">Your story, our canvas</h2>
+                    </div>
+                    <p class="inner-pages-paragraph">At Global Publishing Agency, we understand that the journey of a book begins with its cover – the visual gateway to your narrative world. Our Book Cover Design Services are a symphony of creativity, craftsmanship, and a deep understanding of your story. Immerse yourself in the artistry of our designs, where every cover is a visual narrative crafted to leave a lasting impression. Our book cover designs are not just templates; they are bespoke creations tailored to reflect the essence and individuality of your narrative. 
+                    </p>
+
+                    <div class="inner-pages-btn">
+                        <a href="javascript:void(Tawk_API.toggle())" class="thm-btn">Get A Free Quote</a>
+                    </div>
+                </div>
+            </div>
+            <!--End Welcome One Left-->
+
+            <!--Start Welcome One Right-->
+            <div class="col-xl-6">
+                <div class="welcome-one__right clearfix">
+                    <div class="shape1 rotate-me"><img src="assets/images/shapes/thm-shape1.png" alt=""></div>
+                    <div class="wow slideInRight animated" data-wow-delay="100ms" data-wow-duration="2500ms" style="visibility: visible; animation-duration: 2500ms; animation-delay: 100ms; animation-name: slideInRight;">
+                        <div class="inner-pages-img">
+                            <img src="assets/images/researcher2.jpg" alt="">
+                        </div>
+                    </div>
+                    <!-- <div class="welcome-one__right-img2 wow zoomIn animated" data-wow-delay="100ms" data-wow-duration="3500ms" style="visibility: visible; animation-duration: 3500ms; animation-delay: 100ms; animation-name: zoomIn;">
+                        <img src="assets/images/resources/boook-3.png" alt="">
+                    </div> -->
+                </div>
+            </div>
+            <!--End Welcome One Right-->
+        </div>
+    </div>
+</section>
+
+<!--Features One Start-->
+<section class="features-two bg-paper-pub">
+    <div class="inner-section text-center">
+        <span class="section-title__tagline">We Have Experts For All</span>
+        <h2 class="inner-pages-title-white">Crafting your book's visual blueprint: our process unveiled</h2>
+    </div>
+    <div class="container">
+        <div class="row justify-content-center ">
+            <!--Start Single Features One-->
+            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
+                <div class="features-one__single">
+                    <div class="features-one__single-icon">
+                        <span class="icon-empowerment"></span>
+                    </div>
+                    <div class="features-one__single-text">
+                        <h4><a href="#!">Consultation</a></h4>
+                        <p>
+                            Our journey together begins with a comprehensive consultation, a crucial phase where we immerse ourselves in the essence of your book. We explore its genre, themes, and, most importantly, your vision for the cover. This in-depth understanding lays the foundation for a design that seamlessly aligns with the core of your narrative.</p>
+                            <br><br>
+                    </div>
+                </div>
+            </div>
+            <!--End Single Features One-->
+
+            <!--Start Single Features One-->
+            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1500ms">
+                <div class="features-one__single">
+                    <div class="features-one__single-icon">
+                        <span class="icon-human-resources-1"></span>
+                    </div>
+                    <div class="features-one__single-text">
+                        <h4><a href="#!">Concept Development</a></h4>
+                        <p>
+                            Your insights guide us as we collectively shape the visual blueprint of your literary masterpiece. With the insights gathered, our team of skilled designers takes the helm to transform your ideas into an eye-catcher of a cover. This stage empowers you to choose the design that portrays your story and aligns with aesthetic preferences.</p>
+                            <br><br>
+                    </div>
+                </div>
+            </div>
+            <!--End Single Features One-->
+
+            <!--Start Single Features One-->
+            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="400ms" data-wow-duration="1500ms">
+                <div class="features-one__single">
+                    <div class="features-one__single-icon">
+                        <span class="icon-recruitment"></span>
+                    </div>
+                    <div class="features-one__single-text">
+                        <h4><a href="#!">Refinement</a></h4>
+                        <p>
+                            We work in close partnership with you to refine the chosen design, ensuring that every nuance aligns precisely with your expectations. This iterative process is a testament to our commitment to perfection, guaranteeing a cover that not only looks aesthetically pleasing but, more importantly, feels intimately connected to the essence of your narrative.</p>
+                    </div>
+                </div>
+            </div>
+            <!--End Single Features One-->
+
+            <!--Start Single Features One-->
+            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1500ms">
+                <div class="features-one__single">
+                    <div class="features-one__single-icon">
+                        <span class="icon-human-resources-1"></span>
+                    </div>
+                    <div class="features-one__single-text">
+                        <h4><a href="#!">Finalization</a></h4>
+                        <p>
+                            As the chosen design undergoes thorough refinement, we move towards the finalization stage. Beyond aesthetics, we ensure that the cover meets and exceeds industry standards for both print and digital formats. The culmination of this process is a cover that captures attention and transforms into a powerful marketing tool.</p>
+                        <br><br>
+                    </div>
+                </div>
+            </div>
+            <!--End Single Features One-->
+
+            <!--Start Single Features One-->
+            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="400ms" data-wow-duration="1500ms">
+                <div class="features-one__single">
+                    <div class="features-one__single-icon">
+                        <span class="icon-recruitment"></span>
+                    </div>
+                    <div class="features-one__single-text">
+                        <h4><a href="#!">Premium Editing</a></h4>
+                        <p>
+                            Our premium editing service is designed to improve the overall quality of your research paper. Our team of experienced editors reviews your paper for grammar, spelling, syntax, and style errors. We also provide feedback on the structure, organization, and clarity of your paper to ensure that it meets the highest academic standards.</p> <br>
+                    </div>
+                </div>
+            </div>
+            <!--End Single Features One-->
+
+            <!--Start Single Features One-->
+            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="400ms" data-wow-duration="1500ms">
+                <div class="features-one__single">
+                    <div class="features-one__single-icon">
+                        <span class="icon-recruitment"></span>
+                    </div>
+                    <div class="features-one__single-text">
+                        <h4><a href="#1">Pre-submission Peer Review</a></h4>
+                        <p>
+                            Getting feedback from experts in your field can significantly improve the quality and impact of your research paper. Our pre-submission peer review service involves having your paper reviewed by a panel of experts in your research area. They provide feedback on the originality, clarity, and relevance of your research, helping you to improve your paper before submission.</p>
+                    </div>
+                </div>
+            </div>
+            <!--End Single Features One-->
+        </div>
+    </div>
+</section>
+<!--Features One End-->
+
+<!--Content Section-->
+<section class="about-two">
+    <div class="container">
+        <div class="about-two__bottom-content plain-cnt">
+            <div class="row">
+                <div class="col-xl-7 col-lg-7">
+                    <div class="about-two__title-box">
+                        <div class="inner-section">
+                            <span class="section-title__tagline">Professional Publishing Services </span>
+                            <h2 class="inner-pages-title">Why choose our book cover design services?
+                            </h2>
+                        </div>
+
+                        <p class="inner-pages-paragraph">Your book cover design journey with us is a collaborative masterpiece – a fusion of your narrative brilliance and our design expertise. With a keen eye for design trends, a passion for storytelling, and a commitment to excellence, our designers transform concepts into captivating visuals that make your book stand out on the shelves. From genre-specific elements to capturing the emotional tone, our designs ensure that your cover is as captivating as the story it encases</p>
+                    </div>
+
+                    <ul class="list-unstyled">
+                        <li class="why-choose-one__left-list-single">
+                            <div class="icon">
+                                <span class="icon-confirmation"></span>
+                            </div>
+                            <div class="text">
+                                <p>On-time delivery.</p>
+                            </div>
+                        </li>
+
+                        <li class="why-choose-one__left-list-single">
+                            <div class="icon">
+                                <span class="icon-confirmation"></span>
+                            </div>
+                            <div class="text">
+                                <p>24/7 support and assistance.</p>
+                            </div>
+                        </li>
+
+                        <li class="why-choose-one__left-list-single">
+                            <div class="icon">
+                                <span class="icon-confirmation"></span>
+                            </div>
+                            <div class="text">
+                                <p>Affordable packages.</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
+
+                <div class="col-xl-5 col-lg-5">
+                    <div class="domain-search-wrap border gray-light-bg" style="margin: 0;">
+                        <h4 class="text-center">Register Now | 50% Discount?</h4>
+                        <div class="form-banner">
+                            <div class="the-form-banner">
+    <div class="the-form_head">
+        <!-- <h2 class="pulse">EARLY BIRD DISCOUNTS</h2>
+        <h5>We are here to help you ace your academics.</h5> -->
+    </div>
+    <?php include "./components/registrationform.php"?>
+</section>
+<!--About Two End-->
+
+<!--Portfolio Start-->
+
+<?php include "./components/portprocess.php"?> 
+
+<!--Portfolio Start-->
+
+
+<!--<div class="floating_btn">-->
+<!--    <a target="_blank" href="https://wa.me/+16164693925/?text=Hello, I need help in my publications">-->
+<!--      <div class="contact_icon">-->
+<!--           <img src="assets/images/whatsapp.png" class="my-float" alt="">-->
+
+<!--      </div>-->
+<!--</a></div>-->
+
+
+<!--Start Footer One-->
+<?php include "./components/footer.php"?> 
+<!--End Footer One-->
+</body>
+
+</html>
