6bd9f77b298fc9ff428930e5b1d1dd080c28d1e1
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-01-04 20:25:36 +0500

    update

--- book-writing-service.php
+++ book-writing-service.php
@@ -228,75 +228,11 @@
 </section>
 <!--About Two End-->
 
+<!--Portfolio Start-->
 
+<?php include "./components/portprocess.php"?> 
 
-<section class="books-portfolio-section">
-    <div class="container">
-        <div class="row">
-
-            <div class="inner-section text-center">
-                <span class="section-title__tagline-white">Our Portfolio </span>
-                <h2 class="inner-pages-title-white">Some Of Global Publishing Successful Creations</h2>
-                <p>Our portfolio showcases some of the successful projects we have completed for our clients, demonstrating our expertise and commitment to excellence.</p>
-            </div>
-            <div class="col-12 col-lg-3 col-md-3 col-sm-12">
-                <img src="assets/images/books/1.png" class="book-img" alt="">
-            </div>
-
-            <div class="col-12 col-lg-3 col-md-3 col-sm-12">
-                <img src="assets/images/books/2.png" class="book-img" alt="">
-            </div>
-
-            <div class="col-12 col-lg-3 col-md-3 col-sm-12">
-                <img src="assets/images/books/3.png" class="book-img" alt="">
-            </div>
-
-            <div class="col-12 col-lg-3 col-md-3 col-sm-12">
-                <img src="assets/images/books/4.png" class="book-img" alt="">
-            </div>
-
-            <div class="col-12 col-lg-3 col-md-3 col-sm-12">
-                <img src="assets/images/books/5.png" class="book-img" alt="">
-            </div>
-
-            <div class="col-12 col-lg-3 col-md-3 col-sm-12">
-                <img src="assets/images/books/6.png" class="book-img" alt="">
-            </div>
-
-            <div class="col-12 col-lg-3 col-md-3 col-sm-12">
-                <img src="assets/images/books/7.png" class="book-img" alt="">
-            </div>
-
-            <div class="col-12 col-lg-3 col-md-3 col-sm-12">
-                <img src="assets/images/books/8.png" class="book-img" alt="">
-            </div>
-
-        </div>
-    </div>
-</section>
-
-<section class="our-process pt-50 pb-150">
-    <div class="inner-section text-center mb-120">
-        <span class="section-title__tagline">As simple as 1-2-3</span>
-        <h2 class="inner-pages-title">Our Process </h2>
-    </div>
-    <div class="rt-container">
-        <div class="col-rt-12">
-            <div class="Scriptcontent">
-
-                <!-- partial:index.partial.html -->
-                <ul class="timeline">
-                    <li data-year="1" data-text="1. Consultation: We listen to your ideas and provide personalized solutions."></li>
-                    <li data-year="2" data-text="2. Planning: We create a detailed plan for your project and set milestones."></li>
-                    <li data-year="3" data-text="3. Writing & Editing: Our team of experts helps you craft a compelling story."></li>
-                    <li data-year="4" data-text="4. Formatting & Design: We format and design your work to industry standards."></li>
-                    <li data-year="5" data-text="5. Publication & Promotion: We publish your work and promote it to reach your audience."></li>
-                </ul>
-                <!-- partial -->
-            </div>
-        </div>
-    </div>
-</section>
+<!--Portfolio Start-->
 
 
 <!--<div class="floating_btn">-->
