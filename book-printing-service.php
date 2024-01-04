6b3f739b8b4af096f071eaf1b28e47eeb12ba46b
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-01-04 20:16:59 +0500

    update

--- book-printing-service.php
+++ book-printing-service.php
@@ -8,76 +8,11 @@
 
 <body>
     <!-- /.preloader -->
-    <div class="page-wrapper">
-
-        <header class="main-header main-header--one  clearfix">
-
-            <div class="main-header-one__bottom clearfix">
-                <div class="container">
-                    <div class="main-header-one__bottom-inner clearfix">
-                        <div class="main-header--one__top-left">
-                            <div class="main-header--one__top-logo">
-                                <a href="index.html"><img class="main-logo-img" src="assets/images/logo-v2.png" alt="" /></a>
-                            </div>
-                        </div>
-                        <nav class="main-menu main-menu--1 align-item-center">
-                            <div class="main-menu__inner">
-                                <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
-
-                                <div class="left">
-                                    <ul class="main-menu__list">
-                                        <li class="current">
-                                            <a href="index.html">Home</a>
-                                        </li>
-                                        <li class="dropdown">
-                                            <a href="#!">Services <i class="fas fa-angle-down"></i></a>
-                                            <ul>
-                                                <li><a href="book-writing-service.html">Book Writing &amp; Publishing</a></li>
-                                                <li><a href="book-editing-proofreading-service.html">Book Editing &amp; Proofreading</a></li>
-                                                <li><a href="ghost-writing-service.html">Ghost Book Writing</a></li>
-                                                <li><a href="audio-book-service.html">Audio Book Service</a></li>
-                                                <li><a href="book-cover-designing.html"> Book Cover Design </a></li>
-                                                <li><a href="book-illustrations-designing.html">  Book Illustrations  </a></li>
-                                                <li><a href="book-publishing-service.html">   Book Publishing   </a></li>
-                                                <li><a href="book-printing-service.html">   Book Printing    </a></li>
-                                                <li><a href="book-marketing-service.html">    Book Marketing     </a></li>
-                                                <li><a href="author-website-development.html">    Author Website     </a></li>
-                                                <li><a href="amazon-publishing-service.html">    Amazon Publishing     </a></li>
-                                            </ul>
-                                        </li>
-
-                                        <li><a href="about.html">About Us</a></li>
-                                        <li><a href="reviews.html">Our Reviews</a></li>
-
-
-                                        <li><a href="contact.html">Contact Us</a></li>
-                                    </ul>
-
-
-                                </div>
-
-                                <div class="right">
-                                    <div class="main-menu__right">
-                                        <div class="main-menu__right-cart-search">
-                                            <div class="">
-                                                <a href="javascript:void(Tawk_API.toggle())" class="thm-btn " style="padding: 7px 20px;">Order Now</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-        </header>
-
+    <?php include "./components/header.php"?> 
         <div class="stricky-header stricked-menu main-menu">
             <div class="sticky-header__content">
             </div><!-- /.sticky-header__content -->
-        </div><!-- /.stricky-header -->
-<section class="main-banner-custom bg-service-paper">
+        </div><!-- /.stricky-header --><section class="main-banner-custom bg-service-paper">
     <div class="container">
         <div class="row">
             <div class="col-lg-7 col-md-7">
