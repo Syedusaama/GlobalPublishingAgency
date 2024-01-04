f17087ebf3fe05376b0ad46e81f2c9496f47bf97
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-01-04 20:28:11 +0500

    update

--- ghost-writing-service.php
+++ ghost-writing-service.php
@@ -227,75 +227,11 @@
 </section>
 <!--About Two End-->
 
+<!--Portfolio Start-->
 
+<?php include "./components/portprocess.php"?> 
 
-<section class="books-portfolio-section">
-    <div class="container">
-        <div class="row">
-
-            <div class="inner-section text-center">
-                <span class="section-title__tagline-white">Our Portfolio </span>
-                <h2 class="inner-pages-title-white">Empowering Writers, Composing Narratives – Your Story, Your Success.</h2>
-                <p>Browse through our published books and take a sneak peek into the excellence Global Publishing Agency has to offer!</p>
-            </div>
-            <div class="col-12 col-lg-3 col-md-3 col-sm-12">
-                <img src="assets/images/books/1.png" class="book-img" alt="">
-            </div>
-
-            <div class="col-12 col-lg-3 col-md-3 col-sm-12">
-                <img src="assets/images/books/2.png" class="book-img" alt="">
-            </div>
-
-            <div class="col-12 col-lg-3 col-md-3 col-sm-12">
-                <img src="assets/images/books/3.png" class="book-img" alt="">
-            </div>
-
-            <div class="col-12 col-lg-3 col-md-3 col-sm-12">
-                <img src="assets/images/books/4.png" class="book-img" alt="">
-            </div>
-
-            <div class="col-12 col-lg-3 col-md-3 col-sm-12">
-                <img src="assets/images/books/5.png" class="book-img" alt="">
-            </div>
-
-            <div class="col-12 col-lg-3 col-md-3 col-sm-12">
-                <img src="assets/images/books/6.png" class="book-img" alt="">
-            </div>
-
-            <div class="col-12 col-lg-3 col-md-3 col-sm-12">
-                <img src="assets/images/books/7.png" class="book-img" alt="">
-            </div>
-
-            <div class="col-12 col-lg-3 col-md-3 col-sm-12">
-                <img src="assets/images/books/8.png" class="book-img" alt="">
-            </div>
-
-        </div>
-    </div>
-</section>
-
-<section class="our-process pt-50 pb-150">
-    <div class="inner-section text-center mb-120">
-        <span class="section-title__tagline">As simple as 1-2-3</span>
-        <h2 class="inner-pages-title">Our Process </h2>
-    </div>
-    <div class="rt-container">
-        <div class="col-rt-12">
-            <div class="Scriptcontent">
-
-                <!-- partial:index.partial.html -->
-                <ul class="timeline">
-                    <li data-year="1" data-text="1. Consultation: Initial discussion to understand your vision, ideas, and goals."></li>
-                    <li data-year="2" data-text="2. Setting the blueprint: We assign the project to the right team and set the targets and milestones of the project."></li>
-                    <li data-year="3" data-text="3. Proofreading: We make sure the book is free from all sorts of grammar and structural errors."></li>
-                    <li data-year="4" data-text="4. Layout/typesetting: Once approved, the proofread version is then formatted and designed as per the publishing standards."></li>
-                    <li data-year="5" data-text="5. Publishing and distribution: We make sure your published work reaches its target readers while making it available on a wide range of platforms."></li>
-                </ul>
-                <!-- partial -->
-            </div>
-        </div>
-    </div>
-</section>
+<!--Portfolio Start-->
 
 
 <!--<div class="floating_btn">-->
