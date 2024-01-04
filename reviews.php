5032789c83243aaebd88284eab6682d7c77a6ec5
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-01-04 20:42:29 +0500

    update

--- reviews.php
+++ reviews.php
@@ -9,108 +9,7 @@
 
 <body>
     <!-- /.preloader -->
-    <div class="page-wrapper">
-
-        <header class="main-header main-header--one  clearfix">
-
-            <div class="main-header-one__bottom clearfix">
-                <div class="container">
-                    <div class="main-header-one__bottom-inner clearfix">
-                        <div class="main-header--one__top-left">
-                            <div class="main-header--one__top-logo">
-                                <a href="index.html"><img class="main-logo-img" src="assets/images/logo-v2.png" alt="" /></a>
-                            </div>
-                        </div>
-                        <nav class="main-menu main-menu--1 align-item-center">
-                            <div class="main-menu__inner">
-                                <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
-
-                                <div class="left">
-                                    <ul class="main-menu__list">
-                                        <li class="current">
-                                            <a href="index.html">Home</a>
-                                        </li>
-
-
-                                        <!-- <li class="nav-item dropdown">
-                                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                                All Services
-                                            </a>
-                                            <div class="dropdown-menu megamenu" aria-labelledby="dropdown01">
-                                                <div class="row">
-                                                    <div class="col-md-6 mb-6">
-                                                       <h6><a href="seo-services">SEO Services</a></h6>
-                                                        <ul>
-                                                            <li><a href="/book-writing-service">Book Writing &amp; Publishing</a></li>
-                                                            <li><a href="/book-editing-proofreading-service">Book Editing &amp; Proofreading</a></li>
-                                                            <li><a href="/ghost-writing-service">Ghost Book Writing</a></li>
-                                                            <li><a href="/audio-book-service">Audio Book Service</a></li>
-                                                            <li><a href="/book-cover-designing"> Book Cover Design </a></li>
-                                                            <li><a href="/book-illustrations-designing">  Book Illustrations  </a></li>
-                                                        </ul>
-                                                    </div>
-                                                    <div class="col-md-6 mb-6">
-                                                        <h6><a href="ppc-campaign-management-services">Google Paid Advertisement</a></h6>
-                                                        <ul>
-                                                            <li><a href="/book-publishing-service">   Book Publishing   </a></li>
-                                                            <li><a href="/book-printing-service">   Book Printing    </a></li>
-                                                            <li><a href="/book-marketing-service">    Book Marketing     </a></li>
-                                                            <li><a href="/author-website-development">    Author Website     </a></li>
-                                                            <li><a href="/amazon-publishing-service">    Amazon Publishing     </a></li>
-                                                        </ul>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </li> -->
-                                        <li class="dropdown">
-                                            <a href="#!">Services <i class="fas fa-angle-down"></i></a>
-                                            <ul>
-                                                <li><a href="book-writing-service.html">Book Writing &amp; Publishing</a></li>
-                                                <li><a href="book-editing-proofreading-service.html">Book Editing &amp; Proofreading</a></li>
-                                                <li><a href="ghost-writing-service.html">Ghost Book Writing</a></li>
-                                                <li><a href="audio-book-service.html">Audio Book Service</a></li>
-                                                <li><a href="book-cover-designing.html"> Book Cover Design </a></li>
-                                                <li><a href="book-illustrations-designing.html">  Book Illustrations  </a></li>
-                                                <li><a href="book-publishing-service.html">   Book Publishing   </a></li>
-                                                <li><a href="book-printing-service.html">   Book Printing    </a></li>
-                                                <li><a href="book-marketing-service.html">    Book Marketing     </a></li>
-                                                <li><a href="author-website-development.html">    Author Website     </a></li>
-                                                <li><a href="amazon-publishing-service.html">    Amazon Publishing     </a></li>
-                                            </ul>
-                                        </li>
-
-                                        <li><a href="about.html">About Us</a></li>
-                                        <li><a href="reviews.html">Our Reviews</a></li>
-
-
-                                        <li><a href="contact.html">Contact Us</a></li>
-                                    </ul>
-
-
-                                </div>
-
-                                <div class="right">
-                                    <div class="main-menu__right">
-                                        <div class="main-menu__right-cart-search">
-                                            <div class="">
-                                                <a href="javascript:void(Tawk_API.toggle())" class="thm-btn " style="padding: 7px 20px;">Order Now</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-        </header>
-
-        <div class="stricky-header stricked-menu main-menu">
-            <div class="sticky-header__content">
-            </div><!-- /.sticky-header__content -->
-        </div><!-- /.stricky-header -->
-
+    <?php include "./components/header.php"?> 
 <section class="main-banner-custom bg-reviews">
     <div class="container">
         <div class="row">
@@ -133,7 +32,6 @@
         </div>
     </div>
 </section>
-
 
 <div class="fluid">
     <div class="row">
