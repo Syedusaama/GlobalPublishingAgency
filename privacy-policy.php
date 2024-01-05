2b36389d49eb996733d65a0b5345845b4d7581c5
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-01-05 17:55:39 +0500

    update

--- privacy-policy.php
+++ privacy-policy.php
@@ -8,70 +8,7 @@
 
 <body>
     <!-- /.preloader -->
-    <div class="page-wrapper">
-
-        <header class="main-header main-header--one  clearfix">
-
-            <div class="main-header-one__bottom clearfix">
-                <div class="container">
-                    <div class="main-header-one__bottom-inner clearfix">
-                        <div class="main-header--one__top-left">
-                            <div class="main-header--one__top-logo">
-                                <a href="index.html"><img src="assets/images/logo-v2.png" alt="" /></a>
-                            </div>
-                        </div>
-                        <nav class="main-menu main-menu--1">
-                            <div class="main-menu__inner">
-                                <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
-
-                                <div class="left">
-                                    <ul class="main-menu__list">
-                                        <li class="current">
-                                            <a href="index.html">Home</a>
-                                        </li>
-
-                                        <li class="dropdown">
-                                            <a href="#!">Services <i class="fas fa-angle-down"></i></a>
-                                            <ul>
-                                                <li><a href="ebook-publication.html">e-Book Publication</a></li>
-                                                <li><a href="paper-publication.html">Paper Publication</a></li>
-                                                <li><a href="ghostwriting.html">Ghostwriting</a></li>
-                                            </ul>
-                                        </li>
-
-                                        <li><a href="about.html">About Us</a></li>
-                                        <li><a href="reviews.html">Our Reviews</a></li>
-
-
-                                        <li><a href="contact.html">Contact Us</a></li>
-                                    </ul>
-
-
-                                </div>
-
-                                <div class="right">
-                                    <div class="main-menu__right">
-                                        <div class="main-menu__right-cart-search">
-                                            <div class="">
-                                                <a href="javascript:void(Tawk_API.toggle())" class="thm-btn " style="padding: 7px 20px;">Order Now</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-        </header>
-
-        <div class="stricky-header stricked-menu main-menu">
-            <div class="sticky-header__content">
-            </div><!-- /.sticky-header__content -->
-        </div><!-- /.stricky-header -->
-
-
+    <?php include "./components/header.php"?> 
 <section class="main-banner-custom bg-about">
     <div class="container">
         <div class="row">
