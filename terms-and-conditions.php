acbff6d802188daaa6632cf33ca209cc0d1d1223
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-01-05 17:59:32 +0500

    update

--- terms-and-conditions.php
+++ terms-and-conditions.php
@@ -88,12 +88,9 @@
 <!--      </div>-->
 <!--</a></div>-->
 
-
 <!--Start Footer One-->
 <?php include "./components/footer.php"?> 
 <!--End Footer One-->
 </body>
 
-
-<!-- Mirrored from globalpublishingcenter.com/terms-and-conditions by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 15 Dec 2023 09:03:15 GMT -->
 </html>
