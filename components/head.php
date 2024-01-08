1b98921ce3e98b5b338367654b88f88a1f6bea41
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-01-08 21:37:31 +0500

    update

--- components/head.php
+++ components/head.php
@@ -12,7 +12,7 @@
     <!------ ReCaptcha HEAD tag --------
 
 
-    <!-- fonts -->
+    <!- fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com/">
     <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300;400;700&amp;display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
--- components/header.php
+++ components/header.php
@@ -1,7 +1,5 @@
 <div class="page-wrapper">
-
-<header class="main-header main-header--one  clearfix">
-
+    <header class="main-header main-header--one  clearfix">
     <div class="main-header-one__bottom clearfix">
         <div class="container">
             <div class="main-header-one__bottom-inner clearfix">
@@ -13,44 +11,11 @@
                 <nav class="main-menu main-menu--1 align-item-center">
                     <div class="main-menu__inner">
                         <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
-
                         <div class="left">
                             <ul class="main-menu__list">
                                 <li class="current">
                                     <a href="index.html">Home</a>
                                 </li>
-
-
-                                <!-- <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        All Services
-                                    </a>
-                                    <div class="dropdown-menu megamenu" aria-labelledby="dropdown01">
-                                        <div class="row">
-                                            <div class="col-md-6 mb-6">
-                                               <h6><a href="seo-services">SEO Services</a></h6>
-                                                <ul>
-                                                    <li><a href="/book-writing-service">Book Writing &amp; Publishing</a></li>
-                                                    <li><a href="/book-editing-proofreading-service">Book Editing &amp; Proofreading</a></li>
-                                                    <li><a href="/ghost-writing-service">Ghost Book Writing</a></li>
-                                                    <li><a href="/audio-book-service">Audio Book Service</a></li>
-                                                    <li><a href="/book-cover-designing"> Book Cover Design </a></li>
-                                                    <li><a href="/book-illustrations-designing">  Book Illustrations  </a></li>
-                                                </ul>
-                                            </div>
-                                            <div class="col-md-6 mb-6">
-                                                <h6><a href="ppc-campaign-management-services">Google Paid Advertisement</a></h6>
-                                                <ul>
-                                                    <li><a href="/book-publishing-service">   Book Publishing   </a></li>
-                                                    <li><a href="/book-printing-service">   Book Printing    </a></li>
-                                                    <li><a href="/book-marketing-service">    Book Marketing     </a></li>
-                                                    <li><a href="/author-website-development">    Author Website     </a></li>
-                                                    <li><a href="/amazon-publishing-service">    Amazon Publishing     </a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li> -->
                                 <li class="dropdown">
                                     <a href="#!">Services <i class="fas fa-angle-down"></i></a>
                                     <ul>
@@ -67,17 +32,11 @@
                                         <li><a href="amazon-publishing-service.html">    Amazon Publishing     </a></li>
                                     </ul>
                                 </li>
-
                                 <li><a href="about.html">About Us</a></li>
                                 <li><a href="reviews.html">Our Reviews</a></li>
-
-
                                 <li><a href="contact.html">Contact Us</a></li>
                             </ul>
-
-
                         </div>
-
                         <div class="right">
                             <div class="main-menu__right">
                                 <div class="main-menu__right-cart-search">
@@ -87,13 +46,13 @@
                                 </div>
                             </div>
                         </div>
-
                     </div>
                 </nav>
             </div>
         </div>
     </div>
-</header>
+    </header>
+</div>
 
 <div class="stricky-header stricked-menu main-menu">
     <div class="sticky-header__content">
