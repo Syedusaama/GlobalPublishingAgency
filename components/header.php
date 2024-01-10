c0f1e75af347e38e2e5c0483601f090fb2d4654b
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-01-10 13:06:44 +0500

    update

--- components/header.php
+++ components/header.php
@@ -51,47 +51,9 @@
             </div>
         </div>
     </div>
-</header>
+    </header>
 
-<div class="stricky-header stricked-menu main-menu stricky-fixed">
-            <div class="sticky-header__content">
-                            <div class="main-menu__inner">
-                                <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
-                                <div class="left">
-                                    <ul class="main-menu__list">
-                                        <li class="current">
-                                            <a href="index.html">Home</a>
-                                        </li>
-                                        <li class="dropdown">
-                                            <a href="#!">Services <i class="fas fa-angle-down"></i></a>
-                                            <ul>
-                                        <li><a href="book-writing-service.php">Book Writing &amp; Publishing</a></li>
-                                        <li><a href="book-editing-proofreading-service.php">Book Editing &amp; Proofreading</a></li>
-                                        <li><a href="ghost-writing-service.php">Ghost Book Writing</a></li>
-                                        <li><a href="audio-book-service.php">Audio Book Service</a></li>
-                                        <li><a href="book-cover-designing.php"> Book Cover Design </a></li>
-                                        <li><a href="book-illustrations-designing.php">  Book Illustrations  </a></li>
-                                        <li><a href="book-publishing-service.php">   Book Publishing   </a></li>
-                                        <li><a href="book-printing-service.php">   Book Printing    </a></li>
-                                        <li><a href="book-marketing-service.php">    Book Marketing     </a></li>
-                                        <li><a href="author-website-development.php">    Author Website     </a></li>
-                                        <li><a href="amazon-publishing-service.php">    Amazon Publishing     </a></li>
-                                    </ul>
-                                        </li>
-                                        <li><a href="about.php">About Us</a></li>
-                                        <li><a href="reviews.php">Our Reviews</a></li>
-                                        <li><a href="contact.php">Contact Us</a></li>
-                                    </ul>
-                                </div>
-                                <div class="right">
-                                    <div class="main-menu__right">
-                                        <div class="main-menu__right-cart-search">
-                                            <div class="">
-                                                <a href="javascript:void(Tawk_API.toggle())" class="thm-btn " style="padding: 7px 20px;">Order Now</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-        </div>
+<div class="stricky-header stricked-menu main-menu">
+    <div class="sticky-header__content">
+    </div><!-- /.sticky-header__content -->
+</div><!-- /.stricky-header -->
