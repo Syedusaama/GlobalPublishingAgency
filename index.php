0e2d7d33175f0c8ae0d29026aaec146d39e9dfa9
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-03-01 17:51:29 +0500

    update

--- index.php
+++ index.php
@@ -27,30 +27,7 @@
 									<?php include "./components/registrationform.php"?>
 		</section>
 		<!--Why Choose One Start-->
-		<section>
-					<div class="row-fluid">
-						<div class="logos-container-2 "> 
-					<div class="container cont-logos h-100 p-3">
-			<div class="row align-items-center h-100">
-			<div class="container rounded">
-				<div class="slider">
-				<div class="logos">
-				<img src="assets/images/resources/1.png" alt="">
-				<img src="assets/images/resources/2.png" alt="">
-				<img src="assets/images/resources/3.png" alt="">
-				<img src="assets/images/resources/4.png" alt="">
-				<img src="assets/images/resources/5.png" alt="">
-				<img src="assets/images/resources/1.png" alt="">
-				<img src="assets/images/resources/2.png" alt="">
-				<img src="assets/images/resources/3.png" alt="">
-				<img src="assets/images/resources/4.png" alt="">
-				<img src="assets/images/resources/5.png" alt="">
-				</div>
-				</div>
-			</div>
-			</div>
-			</div>
-		</section>
+		<?php include "./components/logos.php"?>
 		<!--Features One Start-->
 		<section class="features-one">
 			<div class="section-title text-center"> <span class="section-title__tagline">Your Book, Our Expertise.</span>
